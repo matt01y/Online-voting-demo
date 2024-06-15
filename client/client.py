b850d92026f6381190490de85873188a2626edd9
RobinMeersman <echteenrobin@gmail.com>
2024-06-15 16:42:59 +0200

    fix: fixed encryption, added nonce

--- client/client.py
+++ client/client.py
@@ -1,3 +1,5 @@
+import random
+
 import requests as req
 import json
 import gnupg
@@ -56,7 +58,7 @@ def user_vote(parties: list[dict[str, str]]) -> tuple[str, str] | None:
             print("[ERROR]: could not cast the input to a number")
 
 
-def send_vote(vote_id: int, vote: tuple[str, str] | None, address: str, user_key, backend_key,
+def send_vote(vote_id: int, vote: tuple[str, str] | None, address: str, user_key, encryption_key,
               encryptor: gnupg.GPG) -> None:
     # (vote_id, sign(vote_id, backend_key(nonce, vote)))
     if vote is None:
@@ -70,11 +72,15 @@ def send_vote(vote_id: int, vote: tuple[str, str] | None, address: str, user_key
             "party": vote[1]
         }
 
-    vote_encrypted = gpg.encrypt(json.dumps(vote_json), recipients=backend_key["fingerprint"]).data.decode("utf-8")
+    nonce = random.randint(100000, 999999)
+    vote_encrypted = gpg.encrypt(json.dumps({"nonce": nonce, "vote": vote_json}),
+                                 recipients=encryption_key["fingerprint"], always_trust=True).data.decode("utf-8")
 
     signed = encryptor.sign(json.dumps({"vote_id": vote_id, "vote": vote_encrypted}),
                             keyid=user_key.fingerprint).data.decode("utf-8")
     response = req.post(f"{address}/vote", json={"vote_id": vote_id, "signed": signed})
+
+    print({"vote_id": vote_id, "signed": signed})
 
     if response.status_code != 200:
         print(f"[ERROR]: {response.status_code} - {response.text}")
@@ -110,7 +116,7 @@ if __name__ == '__main__':
 
     user_vote = user_vote(data["parties"])
     # send_vote(voter_id, user_vote, intermediary_address, key, backend_key=data["backend_key"], encryptor=gpg)
-    send_vote(voter_id, user_vote, intermediary_address, key, backend_key=backend_key, encryptor=gpg)  # temporary
+    send_vote(voter_id, user_vote, intermediary_address, key, encryption_key=backend_key, encryptor=gpg)  # temporary
 
     gpg.delete_keys(key.fingerprint, secret=True, passphrase="")
     shutil.rmtree(path)
